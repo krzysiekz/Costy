d9124b8898e5a35b3a7bd8d8e062179540e8eefb
krzysiekz <krzysiekcrpgfan@gmail.com>
2016-11-11 18:33:36 +0100

    refactored test class + added more test cases

--- core/src/test/java/com/krzysiekz/costy/service/ExpenseCalculatorTest.java
+++ core/src/test/java/com/krzysiekz/costy/service/ExpenseCalculatorTest.java
@@ -2,24 +2,29 @@ package com.krzysiekz.costy.service;
 
 import com.krzysiekz.costy.model.ExpenseProject;
 import com.krzysiekz.costy.model.ExpenseReport;
+import com.krzysiekz.costy.model.ReportEntry;
 import com.krzysiekz.costy.model.User;
 import com.krzysiekz.costy.model.UserExpense;
 import com.krzysiekz.costy.service.impl.DefaultExpenseCalculator;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import java8.util.stream.Collectors;
 import java8.util.stream.StreamSupport;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
-import static org.assertj.core.api.Assertions.tuple;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class ExpenseCalculatorTest {
 
     private ExpenseProject expenseProject;
@@ -41,84 +46,92 @@ public class ExpenseCalculatorTest {
     }
 
     @Test
-    public void shouldCalculateExpensesForOneUser() throws Exception {
-        //given
-        UserExpense johnExpense = createExpense("John", 10, Collections.singletonList("John"));
+    @Parameters(method = "prepareTestCases")
+    public void shouldCreateProperReport(List<UserExpense> expenses, List<String> expectedEntries) {
         //when
-        expenseProject.addExpense(johnExpense);
+        StreamSupport.stream(expenses).forEach(e -> expenseProject.addExpense(e));
         ExpenseReport expenseReport = expenseCalculator.calculate(expenseProject);
         //then
-        assertThat(expenseReport.getEntries()).isEmpty();
+        assertThat(expenseReport.getEntries()).hasSize(expectedEntries.size());
+        assertThat(expenseReport.getEntries()).extracting(ReportEntry::toString).
+                containsAll(expectedEntries);
     }
 
-    @Test
-    public void shouldCalculateExpensesForTwoUsers() throws Exception {
-        //given
-        UserExpense johnExpense = createExpense("John", 50, Arrays.asList("John", "Kate"));
-        UserExpense kateExpense = createExpense("Kate", 20, Arrays.asList("John", "Kate"));
-        //when
-        expenseProject.addExpense(johnExpense);
-        expenseProject.addExpense(kateExpense);
-        ExpenseReport expenseReport = expenseCalculator.calculate(expenseProject);
-        //then
-        assertThat(expenseReport.getEntries()).hasSize(1);
-        assertThat(expenseReport.getEntries()).extracting("sender", "receiver", "amount").
-                contains(tuple(kateExpense.getUser(), johnExpense.getUser(), new BigDecimal("15.000")));
+    private Object prepareTestCases() {
+        return new Object[]{
+                reportForSingleUserShouldBeEmpty(),
+                reportShouldBeValidForTwoUsers(),
+                reportShouldBeEmptyWhenExpensesAreTheSameForAllUsers(),
+                reportShouldBeEmptyWhenExpensesAreTheSameForAllUsers2(),
+                reportShouldBeValidForMultipleUsers(),
+                reportShouldBeValidWhenUserIsNotPayingForAllOtherUsers()
+        };
     }
 
-    @Test
-    public void shouldReturnEmptyReportWhenUsersExpensesAreTheSame() throws Exception {
-        //given
-        UserExpense johnExpense = createExpense("John", 50, Arrays.asList("John", "Kate"));
-        UserExpense kateExpense = createExpense("Kate", 50, Arrays.asList("John", "Kate"));
-        //when
-        expenseProject.addExpense(johnExpense);
-        expenseProject.addExpense(kateExpense);
-        ExpenseReport expenseReport = expenseCalculator.calculate(expenseProject);
-        //then
-        assertThat(expenseReport.getEntries()).isEmpty();
+    private Object[] reportShouldBeValidWhenUserIsNotPayingForAllOtherUsers() {
+        return new Object[]{
+                Arrays.asList(
+                        createExpense("John", 15, Arrays.asList("John", "Kate")),
+                        createExpense("Kate", 30, Arrays.asList("John", "Kate", "Bob")),
+                        createExpense("Bob", 45, Arrays.asList("John", "Kate", "Bob"))
+                ),
+                Arrays.asList("John -> Kate: 2,5",
+                        "Kate -> Bob: 5",
+                        "John -> Bob: 15")
+        };
     }
 
-    @Test
-    public void shouldReturnProperReportForMultipleUsers() {
-        //given
-        UserExpense johnExpense = createExpense("John", 9, Arrays.asList("John", "Kate", "Bob"));
-        UserExpense kateExpense = createExpense("Kate", 18, Arrays.asList("John", "Kate", "Bob"));
-        UserExpense bobExpense = createExpense("Bob", 27, Arrays.asList("John", "Kate", "Bob"));
-        //when
-        expenseProject.addExpense(johnExpense);
-        expenseProject.addExpense(kateExpense);
-        expenseProject.addExpense(bobExpense);
-        ExpenseReport expenseReport = expenseCalculator.calculate(expenseProject);
-        //then
-        assertThat(expenseReport.getEntries()).hasSize(3);
-        assertThat(expenseReport.getEntries()).extracting("sender", "receiver", "amount").
-                containsOnly(
-                        tuple(johnExpense.getUser(), kateExpense.getUser(), new BigDecimal("3.000")),
-                        tuple(johnExpense.getUser(), bobExpense.getUser(), new BigDecimal("6.000")),
-                        tuple(kateExpense.getUser(), bobExpense.getUser(), new BigDecimal("3.000"))
-                );
+    private Object[] reportShouldBeValidForMultipleUsers() {
+        return new Object[]{
+                Arrays.asList(
+                        createExpense("John", 9, Arrays.asList("John", "Kate", "Bob")),
+                        createExpense("Kate", 18, Arrays.asList("John", "Kate", "Bob")),
+                        createExpense("Bob", 27, Arrays.asList("John", "Kate", "Bob"))
+                ),
+                Arrays.asList("John -> Kate: 3",
+                        "Kate -> Bob: 3",
+                        "John -> Bob: 6")
+        };
     }
 
-    @Test
-    public void shouldReturnProperReportWhenExpensesAreNotForAllUsers() {
-        //given
-        UserExpense johnExpense = createExpense("John", 15, Arrays.asList("John", "Kate"));
-        UserExpense kateExpense = createExpense("Kate", 30, Arrays.asList("John", "Kate", "Bob"));
-        UserExpense bobExpense = createExpense("Bob", 45, Arrays.asList("John", "Kate", "Bob"));
-        //when
-        expenseProject.addExpense(johnExpense);
-        expenseProject.addExpense(kateExpense);
-        expenseProject.addExpense(bobExpense);
-        ExpenseReport expenseReport = expenseCalculator.calculate(expenseProject);
-        //then
-        assertThat(expenseReport.getEntries()).hasSize(3);
-        assertThat(expenseReport.getEntries()).extracting("sender", "receiver", "amount").
-                containsOnly(
-                        tuple(johnExpense.getUser(), kateExpense.getUser(), new BigDecimal("2.500")),
-                        tuple(johnExpense.getUser(), bobExpense.getUser(), new BigDecimal("15.000")),
-                        tuple(kateExpense.getUser(), bobExpense.getUser(), new BigDecimal("5.000"))
-                );
+    private Object[] reportShouldBeEmptyWhenExpensesAreTheSameForAllUsers() {
+        return new Object[]{
+                Arrays.asList(
+                        createExpense("John", 50, Arrays.asList("John", "Kate")),
+                        createExpense("Kate", 50, Arrays.asList("John", "Kate"))
+                ),
+                new ArrayList<>()
+        };
+    }
+
+    private Object[] reportShouldBeEmptyWhenExpensesAreTheSameForAllUsers2() {
+        return new Object[]{
+                Arrays.asList(
+                        createExpense("John", 50, Arrays.asList("John", "Kate", "Bob")),
+                        createExpense("Kate", 50, Arrays.asList("John", "Kate", "Bob")),
+                        createExpense("Bob", 50, Arrays.asList("John", "Kate", "Bob"))
+                ),
+                new ArrayList<>()
+        };
+    }
+
+    private Object[] reportShouldBeValidForTwoUsers() {
+        return new Object[]{
+                Arrays.asList(
+                        createExpense("John", 50, Arrays.asList("John", "Kate")),
+                        createExpense("Kate", 20, Arrays.asList("John", "Kate"))
+                ),
+                Collections.singletonList("Kate -> John: 15")
+        };
+    }
+
+    private Object[] reportForSingleUserShouldBeEmpty() {
+        return new Object[]{
+                Collections.singletonList(
+                        createExpense("John", 10, Collections.singletonList("John"))
+                ),
+                new ArrayList<>()
+        };
     }
 
     private UserExpense createExpense(String userName, int amount, List<String> receivers) {
